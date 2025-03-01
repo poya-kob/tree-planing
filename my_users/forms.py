c4b10cf651ba91d59ba0f23ac7ed19ec18c9e064
poya <poya_kob@live.com>
2025-03-01 19:29:57 +0330

    register_tree style changed

--- my_users/forms.py
+++ my_users/forms.py
@@ -2,20 +2,29 @@ from django import forms
 
 
 class UsersFrom(forms.Form):
-    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label="موبایل")
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label="نام")
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
-                                label="نام خانوادگی")
-    stage = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
-                            label='مقطع تحصیلی')
-    school = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label='نام مدرسه')
-    zone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label='منطقه')
-    meli_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label='کدملی')
-    birthday = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control form-control-lg'}),
-                               label='تاریخ تولد')
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'موبایل'}),
+                            label="موبایل")
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'نام'}),
+                                 label="نام")
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'نام خانوادگی'}),
+        label="نام خانوادگی")
+    stage = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'مقطع تحصیلی'}),
+        label='مقطع تحصیلی')
+    school = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'نام مدرسه'}),
+                             label='نام مدرسه')
+    zone = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'منطقه'}),
+                           label='منطقه')
+    meli_code = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': 'کدملی'}),
+                                label='کدملی')
+    birthday = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'formbold-form-input', 'placeholder': 'تاریخ تولد'}),
+        label='تاریخ تولد')
 
 
 class LoginUserForm(forms.Form):
-    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), label="موبایل")
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control form-control-lg'}),
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'formbold-form-input', 'placeholder': ''}),
+                            label="موبایل")
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'formbold-form-input', 'placeholder': ''}),
                                label='رمز ورود')
--- my_users/urls.py
+++ my_users/urls.py
@@ -3,7 +3,7 @@ from .views import register_tree, login_view, contact, register
 
 urlpatterns = [
     path('register/<uuid:qr_id>/', register_tree, name='register-tree'),
-    path('register/', register),
+    path('register/', register, name='register'),
     path('login/', login_view, name='login-view'),
     path('contact/', contact, name='contact'),
 
