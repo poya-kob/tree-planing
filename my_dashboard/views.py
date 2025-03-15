1a73df75f0f1853a8fcd3504457fe422de1e1757
poya <poya_kob@live.com>
2025-03-15 19:14:04 +0330

    main dashboard theme added

--- my_dashboard/views.py
+++ my_dashboard/views.py
@@ -13,7 +13,7 @@ class UnapprovedImagesView(ListView):
     model = TreeImage
     template_name = "./dashboard/admin_image_list.html"
     context_object_name = "images"
-    paginate_by = 8  # تعداد عکس‌ها در هر صفحه
+    paginate_by = 9  # تعداد عکس‌ها در هر صفحه
 
     def get_queryset(self):
         return self.model.objects.filter(is_active=False)
@@ -25,7 +25,8 @@ class ApproveImagesView(View):
         if not approved_image_ids:
             messages.warning(request, "هیچ تصویری برای تأیید انتخاب نشده است.")
             return redirect("unapproved-images")
-
+        print(approved_image_ids)
+        print(request.POST)
         images_updated = TreeImage.objects.filter(id__in=approved_image_ids, is_active=False).update(is_active=True)
         messages.success(request, f"{images_updated} تصویر تأیید شد.")
 
@@ -79,4 +80,7 @@ class TreeDetailView(DetailView):
 
 @login_required
 def user_dashboard(request):
-    return render(request, './dashboard/user-dashboard.html')
+    active_trees = QRCode.objects.filter(is_registered=True).count()
+    trees_pic = TreeImage.objects.all().count()
+    context = {'active_trees': active_trees, 'trees_pic': trees_pic}
+    return render(request, './dashboard/user-dashboard.html', context)
